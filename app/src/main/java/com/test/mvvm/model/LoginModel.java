6009ed6a205c16e63cfaa2e847e78bf6217aae54
Murari Meena <murarimeenaji@gmail.com>
2020-04-20 00:35:48 +0530

    model created

---
+++ app/src/main/java/com/test/mvvm/model/LoginModel.java
@@ -0,0 +1,31 @@
+package com.test.mvvm.model;
+
+public class LoginModel {
+    String name;
+    String userid;
+    String paqssword;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPaqssword() {
+        return paqssword;
+    }
+
+    public void setPaqssword(String paqssword) {
+        this.paqssword = paqssword;
+    }
+}
