3eb0da941d842cb55d1a4346435799ac4d1f2837
Murari Meena <murari.meena@ikano.se>
2022-02-16 16:41:34 +0530

    some changes

---
+++ app/src/main/java/com/test/mvvm/utils/Bumblebee.java
@@ -0,0 +1,10 @@
+package com.test.mvvm.utils;
+
+public class Bumblebee {
+
+    private static void featureOne() {
+        System.out.println("feature one");
+        System.out.println("loremipsum");
+    }
+
+}
