e539d21395f4b3eec458ce9a9587139cfe68ac19
Murari Meena <murari.meena@ikano.se>
2022-02-16 16:44:24 +0530

    added new method

--- app/src/main/java/com/test/mvvm/utils/Bumblebee.java
+++ app/src/main/java/com/test/mvvm/utils/Bumblebee.java
@@ -5,6 +5,11 @@ public class Bumblebee {
     private static void featureOne() {
         System.out.println("feature one");
         System.out.println("loremipsum");
+        callAnotherMethod();
+    }
+
+    private static void callAnotherMethod() {
+        System.out.println("method info");
     }
 
 }
